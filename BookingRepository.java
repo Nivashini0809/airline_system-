02d708eda2adbe70017922584d01ce75775f8dce
Nivashini0809 <149260141+Nivashini0809@users.noreply.github.com>
2025-02-24 13:53:10 +0530

    "Initial commit"

---
+++ BookingRepository.java
@@ -0,0 +1,9 @@
+package com.skylink.airlines.repository;
+
+import com.skylink.airlines.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+}
